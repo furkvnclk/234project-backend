fb3a1eb9f139d270fb2be97e0ac30f191af57fc3
Unknown <furkvnclk@gmail.com>
2018-05-10 00:03:43 +0700

    Product class - Create getter, setter, constructor, hashcode and equals

--- src/main/java/camt/se234/project/entity/Product.java
+++ src/main/java/camt/se234/project/entity/Product.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -24,5 +25,77 @@ public class Product {
     String description;
     String imageLocation;
     double price;
+
+    public Product(String s, String apple, String kind_of_fruit, String location, double v) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 &&
+                Objects.equals(id, product.id) &&
+                Objects.equals(productId, product.productId) &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(description, product.description) &&
+                Objects.equals(imageLocation, product.imageLocation);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, productId, name, description, imageLocation, price);
+    }
+
+    public String getDescription() {
+        return description;
+
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageLocation() {
+        return imageLocation;
+    }
+
+    public void setImageLocation(String imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 
 }
